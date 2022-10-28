3689efdf2b4aa3133c17afd1656020416aae6efe
RRafiulSonet <rafiul15-14561@diu.edu.bd>
2022-10-28 15:47:41 +0600

    first commit

---
+++ p.js
@@ -0,0 +1,8 @@
+
+console.log(1);
+document.getElementById("d").innerHTML = 2;
+document.write('potjji')
+let a,b,c;
+a=12; b=30; c=a+b;
+document.getElementById('d1').innerHTML =c;
+document.getElementById('d2').innerHTML ='hiii';
